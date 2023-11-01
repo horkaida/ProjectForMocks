1f339b5a0b25222796f77558050e15ab241a55be
olenalipinska <eveforelinska@gmail.com>
2023-11-01 22:58:00 +0200

    Refactor

--- tests.py
+++ tests.py
@@ -2,24 +2,24 @@ from main import main
 import unittest
 from unittest.mock import patch
 
-class MockPockemonTranslator:
+class MockPokemonTranslator:
     def translate(self, text, target_language="en"):
-        return 'bulbasaure1'
+        return 'bulbasaure'
 
 
-class MockPockemonService:
+class MockPokemonService:
     def get_pokemon_info(self, pokemon_name):
         return {"abilities":[{"ability":{"name":"overgrow"}},
                              {"ability":{"name":"chlorophyll"}}],
                 "height":7,
                 "weight":90
                 }
-class TestPockemonMain(unittest.TestCase):
-    @unittest.mock.patch('main.PokemonNameTranslator', MockPockemonTranslator)
-    @unittest.mock.patch('main.PokemonService', MockPockemonService)
+class TestPokemonMain(unittest.TestCase):
+    @unittest.mock.patch('main.PokemonNameTranslator', MockPokemonTranslator)
+    @unittest.mock.patch('main.PokemonService', MockPokemonService)
 
     def test_main(self):
-        main()
+        # main()
         with open('report_template.html') as f:
             result = f.read()
             self.assertIn('bulbasaure', result)
